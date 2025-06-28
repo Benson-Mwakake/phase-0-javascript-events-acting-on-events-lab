ce22ac51a9c0d0d6cd56f512a03121529e481dd0
Benson <bensonmwanake79@gmail.com>
2025-06-28 13:13:44 +0300

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger')
+  let left = parseInt(dodger.style.left) || 0
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger')
+  let left = parseInt(dodger.style.left) || 0
+  if (left < 360) {
+    dodger.style.left = `${left + 5}px`
+  }
+}
